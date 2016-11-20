4362551c1d1739c06ff3fb11bb91182df8fc32f0
vubon <vubon.roy@gmail.com>
2016-11-20 21:32:42 +0600

    add files

---
+++ squareRoot.py
@@ -0,0 +1,7 @@
+#Python Square Root calculator
+#print('Enter your number: ')
+number = float(input('Enter your number: '))
+
+num_sqrt = number ** 0.5
+
+print('The square root of %0.3f is %0.3f'%(number ,num_sqrt))
